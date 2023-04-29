d1ebb4abca23f77f654a385eab5a5e2dbacf1032
sam <sjglover@udel.edu>
2023-04-29 19:04:05 -0400

    Centered all of the form inputs

--- src/components/AddForm.tsx
+++ src/components/AddForm.tsx
@@ -24,54 +24,66 @@ export default function FormAdd(): JSX.Element {
 
     return (
         <form>
-            <label>
-                Business Name:
-                <input
-                    value={values.business}
-                    onChange={handleInputChange}
-                    name="business"
-                />
-            </label>
-            <label>
-                Description:
-                <input
-                    value={values.description}
-                    onChange={handleInputChange}
-                    name="description"
-                />
-            </label>
-            <label>
-                Category:
-                <input
-                    value={values.category}
-                    onChange={handleInputChange}
-                    name="category"
-                />
-            </label>
-            <label>
-                Address:
-                <input
-                    value={values.address}
-                    onChange={handleInputChange}
-                    name="address"
-                />
-            </label>
-            <label>
-                Website URL:
-                <input
-                    value={values.website}
-                    onChange={handleInputChange}
-                    name="website"
-                />
-            </label>
-            <label>
-                Image URL:
-                <input
-                    value={values.website}
-                    onChange={handleInputChange}
-                    name="image"
-                />
-            </label>
+            <div className="form">
+                <label>
+                    Business Name:
+                    <input
+                        value={values.business}
+                        onChange={handleInputChange}
+                        name="business"
+                    />
+                </label>
+            </div>
+            <div className="form">
+                <label>
+                    Description:
+                    <input
+                        value={values.description}
+                        onChange={handleInputChange}
+                        name="description"
+                    />
+                </label>
+            </div>
+            <div className="form">
+                <label>
+                    Category:
+                    <input
+                        value={values.category}
+                        onChange={handleInputChange}
+                        name="category"
+                    />
+                </label>
+            </div>
+            <div className="form">
+                <label>
+                    Address:
+                    <input
+                        value={values.address}
+                        onChange={handleInputChange}
+                        name="address"
+                    />
+                </label>
+            </div>
+            <div className="form">
+                <label>
+                    Website URL:
+                    <input
+                        value={values.website}
+                        onChange={handleInputChange}
+                        name="website"
+                    />
+                </label>
+            </div>
+            <div className="form">
+                <label>
+                    Image URL:
+                    <input
+                        value={values.website}
+                        onChange={handleInputChange}
+                        name="image"
+                    />
+                </label>
+            </div>
             <button type="submit"> Submit </button>
         </form>
     );
