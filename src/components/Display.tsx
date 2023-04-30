0b6bd7bd98d87a9f461dc105f1e631e5e7df4037
Emilie <emiliea@udel.edu>
2023-04-29 22:44:48 -0400

    sorting good

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -25,7 +25,7 @@ import {
     GreenbankAndPhilips,
     MercuryCafe
 } from "../Assets/instances";
-import { Card, Col, Nav, Row } from "react-bootstrap";
+import { Card, Col, Nav, Row, Form } from "react-bootstrap";
 
 export function useSessionStorage<T>(key: string, initialValue: T | (() => T)) {
     const [value, setValue] = useState<T>(() => {
@@ -46,7 +46,7 @@ export function useSessionStorage<T>(key: string, initialValue: T | (() => T)) {
     return [value, setValue] as [typeof value, typeof setValue];
 }
 
-export function DisplayBusinesses(): JSX.Element {
+export function SortingButton(): JSX.Element {
     const [items, setItems] = useSessionStorage<Business[]>("all-items", [
         TerraCafeGrill,
         GreenBoxKitchen,
@@ -72,7 +72,41 @@ export function DisplayBusinesses(): JSX.Element {
         GreenbankAndPhilips,
         MercuryCafe
     ]);
-    setItems;
+    let printed = DisplayBusinesses(items);
+    const [sort, setSort] = useState<string>("");
+    function updateSorting(event: React.ChangeEvent<HTMLSelectElement>) {
+        setSort(event.target.value);
+    }
+    const sortedList = items.filter((item) => item.category.includes(sort));
+    printed = DisplayBusinesses(sortedList);
+    return (
+        <div>
+            <div>
+                <span style={{ justifyContent: "left" }}>Sort By: </span>
+            </div>
+            <div>
+                <Form.Select
+                    value={sort}
+                    onChange={updateSorting}
+                    style={{ width: "350px", height: "70px", fontSize: "30px" }}
+                >
+                    <option>Restaurant</option>
+                    <option>Arts and Entertainment</option>
+                    <option>Health and Wellness</option>
+                    <option>Retail</option>
+                    <option>Creative Services</option>
+                    <option>Black Owned</option>
+                    <option>Women Owned</option>
+                    <option>LGBTQ+ Owned</option>
+                </Form.Select>
+                <br></br>
+                {printed}
+            </div>
+        </div>
+    );
+}
+
+export function DisplayBusinesses(items: Business[]): JSX.Element {
     return (
         <div className="flex-container">
             <Row sm={1} md={3}>
--- src/pages/Browse.tsx
+++ src/pages/Browse.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { DisplayBusinesses } from "../components/Display";
+import { SortingButton } from "../components/Display";
 export function Browse() {
     return (
         <div className="App">
             <br></br>
             <div className="App-header">Browse Local Businesses</div>
             <br></br>
-            <DisplayBusinesses></DisplayBusinesses>
+            <SortingButton></SortingButton>
+            <br></br>
         </div>
     );
 }
